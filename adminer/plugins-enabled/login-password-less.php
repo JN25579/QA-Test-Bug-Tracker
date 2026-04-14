75a72dc4f520ad56f362b8e5814344c87b093180
jordan <jordannicholas188@gmail.com>
2026-04-14 18:00:50 +0100

    use this file to write your commit message. Lines starting with '#' will be ignored, and an empty message aborts the commit.

---
+++ adminer/plugins-enabled/login-password-less.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../plugins/login-password-less.php';
+
+return new AdminerLoginPasswordLess(
+    '$2y$12$8OtD0UQs.3y6EFt1BwHO8urcLwEr6TMZU25AcZazRree91cqeYrkS'
+);
